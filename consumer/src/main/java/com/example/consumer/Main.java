cf3caaa37f00f6a3237751c643a52d0607ffcadd
jnnilsson <jn.jensnilsson@gmail.com>
2024-04-17 14:09:59 +0200

    add scanner and hardcode for asking which location

--- consumer/src/main/java/com/example/consumer/Main.java
+++ consumer/src/main/java/com/example/consumer/Main.java
@@ -2,14 +2,18 @@ package com.example.consumer;
 
 import com.example.service.Forecast;
 
+import java.util.Scanner;
 import java.util.ServiceLoader;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
         ServiceLoader<Forecast> loader = ServiceLoader.load(Forecast.class);
 
         for (Forecast forecast : loader) {
+            System.out.println("Enter location for a weather forecast: ");
+            scanner.nextLine();
             System.out.println(forecast.forecast("Gothenburg"));
         }
 
