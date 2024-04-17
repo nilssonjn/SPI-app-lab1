bdc4608e0fca7466e0a22b1afd3b0a9efb231edc
jnnilsson <jn.jensnilsson@gmail.com>
2024-04-17 15:17:19 +0200

    add AdvancedWeatherForecast with api and menu to choose in main

--- provider/src/main/java/module-info.java
+++ provider/src/main/java/module-info.java
@@ -1,8 +1,9 @@
+import com.example.provider.AdvancedWeatherForecast;
 import com.example.provider.WeatherForecast;
 import com.example.service.Forecast;
 
 module com.example.provider {
     requires com.example.service;
 
-    provides Forecast with WeatherForecast;
+    provides Forecast with WeatherForecast, AdvancedWeatherForecast;
 }
