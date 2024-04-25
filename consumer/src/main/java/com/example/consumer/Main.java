711c7df2ddebc4e706baa1671eea92d08c7c8b21
jnnilsson <jn.jensnilsson@gmail.com>
2024-04-25 11:38:14 +0200

    add loop for menu choice and refactor

--- consumer/src/main/java/com/example/consumer/Main.java
+++ consumer/src/main/java/com/example/consumer/Main.java
@@ -17,23 +17,32 @@ public class Main {
         for (Forecast service : loader) {
             forecastServices.add(service);
         }
+        handleMenuAndSelection(forecastServices, scanner);
+    }
 
-        System.out.println("Choose a weather forecast service:");
-        int index = 1;
-        for (Forecast service : forecastServices) {
-            System.out.println(index++ + ". " + service.getClass().getSimpleName());
-        }
-        int choice = scanner.nextInt();
-        if (choice >= 1 && choice <= forecastServices.size()) {
-            Forecast selectedService = forecastServices.get(choice - 1);
-            System.out.println("Enter location:");
-            scanner.nextLine();
-            String location = scanner.nextLine();
-            String forecast = selectedService.forecast(location);
-            System.out.println(forecast);
-        } else {
-            System.out.println("Invalid choice!");
-        }
+    private static void handleMenuAndSelection(List<Forecast> forecastServices, Scanner scanner) {
+        boolean running = true;
+        while (running) {
+            System.out.println("\nChoose a weather forecast service:");
+            System.out.println("0. Exit");
 
+            int index = 1;
+            for (Forecast service : forecastServices) {
+                System.out.println(index++ + ". " + service.getClass().getSimpleName());
+            }
+            int selection = scanner.nextInt();
+            if (selection == 0) {
+                running = false;
+            } else if (selection >= 1 && selection <= forecastServices.size()) {
+                Forecast selectedService = forecastServices.get(selection - 1);
+                System.out.println("Enter location:");
+                scanner.nextLine();
+                String location = scanner.nextLine();
+                String forecast = selectedService.forecast(location);
+                System.out.println(forecast);
+            } else {
+                System.out.println("Invalid choice!");
+            }
+        }
     }
 }
